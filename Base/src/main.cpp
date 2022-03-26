5486cf5531d2ed78638e0a8b8b9fc4af1802c877
Noah Siebart <nosiebart@gmail.com>
2022-03-26 01:59:46 +0100

    Controller zu Automat konvertiert; Erklärung als Kommentare hinzugefügt

--- Base/src/main.cpp
+++ Base/src/main.cpp
@@ -1,56 +1,54 @@
+//--Erforderliche Bibliotheken--
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WebSocketsServer.h>
 #include <InverseKinematics.h>
 #include <AccelStepper.h>
 
-#define standardG 0
-#define standardB 90
-#define standardT 90
-
+//--WLAN-Netzwerk--
 const char* WIFI_NAME =  "Noah";
 const char* WIFI_PASS = "testtest01";
 
-int posX = 0;
+//--3D-Position--
+int posX = 0; //Min: 0; Max: 1000
 int posY = 0;
 int posZ = 0;
 
-double currentPositionG = standardG; //G = Ground
-double currentPositionB = standardB; // B = Bottom
-double currentPositionT = standardT; //T = Top
-
+//--Websocket--
 WebSocketsServer webSocket = WebSocketsServer(81);
 
-//Stepper motoren sind intern komisch verbunden; in1 und in4 müssen hardware oder software mäßig verändert werden so das in1 -> in4 und in4 -> in1
-//Wir haben dies über Software gemacht
+//--Schrittmotoren--
 AccelStepper stepperBase(AccelStepper::FULL4WIRE, 13, 32, 12, 33);
 AccelStepper stepperBottom(AccelStepper::FULL4WIRE, 25, 27, 26, 14);
 AccelStepper stepperTop(AccelStepper::FULL4WIRE, 19, 5, 18, 17);
 
+//--WebSocket Event--
+//Hier wird direkt die Rotation der Schrittmotoren ausgerechnet und auch angewendet
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
 
     switch(type) {
         case WStype_TEXT: {
-            String coords = reinterpret_cast<char *>(payload);
-            Serial.printf("[%u] get Text: %s\n", num, coords.c_str());
+            String coords = reinterpret_cast<char *>(payload); //Das Payload aus uint_t Array Buchstaben wird in char Array Buchstaben konvertiert, was wir in einem String für weitere Zugriffe speichern
+            Serial.printf("[%u] get Text: %s\n", num, coords.c_str()); //Gibt den String aus, um festzustellen, ob die konvertierung geklappt hat
             if(coords.equalsIgnoreCase("Connected")){
-                //Go to normal position so after restart we know where the motors are
-                //current - standard = steps to make to reach start position
+                //Wenn der Arm "Connected" empfängt gehen die Schrittmotoren auf ihre anfangs Position zurück
+                //Also am Anfang die Position die sie gerade haben
                 stepperBase.moveTo(0);
-                stepperBottom.moveTo(0);
-                stepperTop.moveTo(0);
+                stepperBottom.moveTo(map(90, -180, 180, -256, 256));
+                stepperTop.moveTo(map(90, 0, 180, 512, -512));
             }else {
-                char *charString = strdup(coords.c_str());
-                char *processedString = strtok(charString, ",");
-                posX = map(atoi(processedString), 0, 1000, -175, 175);
-                Serial.printf("%d\n", posX);
-                processedString = strtok(NULL, ",");
-                posY = map(atoi(processedString), 0, 1000, -175, 175);
-                Serial.printf("%d\n", posY);
-                processedString = strtok(NULL, ",");
-                posZ = map(atoi(processedString), 0, 1000, 0, 180);
-                Serial.printf("%d\n", posZ);
-                free(charString);
+                char *charString = strdup(coords.c_str()); //String wird wieder in ein char Array konvertiert damit wir ihn splitten können und die einzelnen X/Y/Z Werte ermitteln können
+                char *processedString = strtok(charString, ","); //char Array wird mit strtok in Teil Arrays aufgesplittet, wobei processedString nun den ersten Wert(X) enthält
+                posX = map(atoi(processedString), 0, 1000, -175, 175); //Zahlen als Buchstaben werden mit atoi in Integer Zahlen konvertiert, map verändert die Werte so, dass sie für Inverse Kinematics passen
+                Serial.printf("%d\n", posX); //Ausgabe zu Testzwecken
+                processedString = strtok(NULL, ","); //Geht weiter zum nächsten Teilstück weswegen kein neuer Pointer, sondern NULL angegeben werden muss
+                posY = map(atoi(processedString), 0, 1000, -175, 175); //Y Wert
+                Serial.printf("%d\n", posY); //Ausgabe zu Testzwecken
+                processedString = strtok(NULL, ","); //Letztes Element
+                posZ = map(atoi(processedString), 0, 1000, 0, 180); //Z Wert
+                Serial.printf("%d\n", posZ); //Ausgabe zu Testzwecken
+                free(charString); //Pointer und die Adresse, auf die der Pointer zeigt, können wieder freigegeben werden
+                //Wendet die Formeln aus "2.2.1. Inverse Kinematics" an
                 double dist = InverseKinematics::getDistance(0,0, posX, posY);
                 Serial.println("Distance: " + String( dist ));
                 double newPosX = InverseKinematics::getDeltaAngle(posX, posY);
@@ -59,12 +57,10 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
                 Serial.println("Upper Angle: " + String( angleOne));
                 double angleTwo = InverseKinematics::getAlphaAngle(dist, posZ, InverseKinematics::lT, InverseKinematics::lB, angleOne);
                 Serial.println("Down Angle: " + String( angleTwo ));
-                stepperBase.moveTo(map(newPosX, -360, 360, -1024, 1024));
-                stepperBottom.moveTo(map(angleTwo, -360, 360, -640, 640));
-                stepperTop.moveTo(map(angleOne, -360, 360, -640, 640));
-                currentPositionG = newPosX;
-                currentPositionB = angleTwo;
-                currentPositionT = angleOne;
+                //AccelStepper Bibliothek setzt die Rotationen/Schritte als Ziel
+                stepperBase.moveTo(map(newPosX, -180, 180, -1024, 1024));
+                stepperBottom.moveTo(map(angleTwo, -180, 180, -256, 256));
+                stepperTop.moveTo(map(angleOne, 0, 180, 512, -512));
             }
             break;
         }
@@ -74,29 +70,37 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
 
 }
 
+//--Setup--
 void setup(){
-    Serial.begin(9600);
+    Serial.begin(9600); //Serial Ausgabe zu Testzwecken
 
+    //WLAN im Access Point Mode initialisieren
     WiFi.mode(WIFI_MODE_AP);
     WiFi.softAP(WIFI_NAME, WIFI_PASS);
 
+    //Websocket Kommunikation vorbereiten
     webSocket.onEvent(webSocketEvent);
     webSocket.begin();
 
+    //Schrittmotoren auf die richtigen Werte einstellen die wir bei unseren Messreihen herausgefunden haben
     stepperBase.setMaxSpeed(12);
     stepperBase.setAcceleration(128);
     stepperBottom.setMaxSpeed(12);
     stepperBottom.setAcceleration(128);
+    stepperBottom.setCurrentPosition(map(90, -180, 180, -256, 256));
     stepperTop.setMaxSpeed(12);
     stepperTop.setAcceleration(128);
+    stepperTop.setCurrentPosition(map(90, 0, 180, 512, -512));
+
 }
 
-
+//--Loop--
 void loop(){
+    //Websocket Kommunikation am laufen halten
     webSocket.loop();
+    //Die Bibliothek guckt, ob die Schrittmotoren ihre Position erreicht haben und bewegt sie, wenn nötig
     stepperBase.run();
     stepperBottom.run();
     stepperTop.run();
-    yield();
 }
 
--- Controller/src/main.cpp
+++ Controller/src/main.cpp
@@ -1,47 +1,71 @@
+//--Erforderliche Bibliotheken--
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <Wire.h>
 #include <ESP8266WiFi.h>
 #include <WebSocketsClient.h>
-#include "../.pio/libdeps/d1_mini/U8g2/src/clib/u8g2.h"
 
+//--WLAN-Netzwerk--
 const char* WIFI_NAME =  "Noah";
 const char* WIFI_PASS = "testtest01";
 
+//--Oled-Display--
+//SSD1306 Display über I2C mit den Hardware standard Ports für I2C
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
+//--Websocket--
 WebSocketsClient webSocket;
+String msg = "";
 
-bool toggle = 1;
+//--Button Variablen--
+//Werden als Button Debounce benutzt
 bool joystickButtonLast = 0;
 bool confirmButtonLast = 0;
 bool resetButtonLast = 0;
 
+//--Positions Variablen--
+//Geschw. des Cursors
 int X = 0;
 int Y = 0;
 
+//Position des Cursors
 int posX = 500; //Min: 0; Max: 1000
 int posY = 500;
 int posZ = 500;
 
+//--Automat Variablen--
+int zustand = 0;
+/* 1: Confirm press
+ * 2: Confirm go
+ * 3: Joystick move
+ * 4: Joystick Button
+ * 5: Reset press
+ * 6: Reset go*/
+int eingabe = 3;
+/* 1: Pos send
+ * 2: Pos reset
+ * 3: show x/y
+ * 4: change x/y
+ * 5: show z
+ * 6: change z */
+int ausgabe = 3;
+
+//--Funktionen--
+//Hier deklariert da die richtige Deklaration erst unter den Stellen stattfindet, wo sie gebraucht werden
 int changeJoyX();
 int changeJoyY();
 int sanitizeInput(int sanInt, int oldInt);
-void convertJoyToPos();
+int g(int zustand, int eingabe);
+int f(int zustand, int eingabe);
 
+//--WebSocket Event--
+//Sobald eine Verbindung aufgebaut ist, wird dem Arm mitgeteilt das der Controller verbunden ist
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
-
     switch(type) {
-        case WStype_DISCONNECTED:
-            Serial.printf("[WSc] Disconnected!\n");
-            break;
         case WStype_CONNECTED: {
             Serial.printf("[WSc] Connected to url: %s\n", payload);
             webSocket.sendTXT("Connected");
         }
-            break;
-        case WStype_TEXT:
-            Serial.printf("[WSc] get text: %s\n", payload);
             break;
         default:
             break;
@@ -49,134 +73,319 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
 
 }
 
-void setup(void){
-    Serial.begin(9600);
-    u8g2.begin();
-    WiFi.begin(WIFI_NAME, WIFI_PASS);
 
-    while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
-        u8g2.setFont(u8g2_font_finderskeepers_tf);	// choose a suitable font
-        u8g2.clearBuffer();
-        u8g2.drawBox(0,0, 128, 9);
-        u8g2.setDrawColor(0);
-        u8g2.drawStr(40,8, "Connecting...");
-        Serial.println("Conection");
-        u8g2.setDrawColor(1);
-        u8g2.sendBuffer();
+//--Setup--
+void setup(void){
+    Serial.begin(9600); //Serial Ausgabe zu Testzwecken
+    u8g2.begin(); //Display initialisieren
+    WiFi.begin(WIFI_NAME, WIFI_PASS); //WLAN im Station Mode initialisieren
+
+    //Warten bis wir über WLAN mit dem Arm verbunden sind
+    while (WiFi.status() != WL_CONNECTED) {
+        //Display zeigt Connecting... an wenn noch nicht verbunden
+        u8g2.setFont(u8g2_font_finderskeepers_tf); //Textart
+        u8g2.clearBuffer(); //Display leer
+        u8g2.drawBox(0,0, 128, 9); //Weiße Box
+        u8g2.setDrawColor(0); //Farbe auf Schwarz setzen
+        u8g2.drawStr(40,8, "Connecting..."); //Text in die Box schreiben
+        Serial.println("Connection"); //Ausgabe zu Testzwecken
+        u8g2.setDrawColor(1); //Farbe wieder auf Weiß setzen
+        u8g2.sendBuffer(); //Frame an das Display senden
         delay(500);
     }
 
+    //Websocket Kommunikation vorbereiten
     webSocket.begin(WiFi.gatewayIP().toString(), 81, "/");
     webSocket.onEvent(webSocketEvent);
     webSocket.setReconnectInterval(5000);
 
-    pinMode(13, INPUT); //Joystick Button
-    pinMode(0, INPUT); //Confirm
-    pinMode(2, INPUT); //Reset
-    X = map(changeJoyX(), 13, 236, 0, 50);
+    pinMode(13, INPUT); //Joystick Button initialisieren
+    pinMode(0, INPUT); //Confirm Button initialisieren
+    pinMode(2, INPUT); //Reset Button initialisieren
+    X = map(changeJoyX(), 13, 236, 0, 50); //Joystick werte initialisieren
     Y =  map(changeJoyY(), 13, 236, 0, 50);
 }
 
+//--Loop--
 void loop(void){
-    X = sanitizeInput(changeJoyX(), X);
-    delay(10);
-    Y = sanitizeInput(changeJoyY(), Y);
-    convertJoyToPos();
+    //Zu Testzwecken Automat-Daten ausgeben
+    Serial.println(zustand);
+    Serial.println(eingabe);
+    Serial.println(ausgabe);
 
-    if(toggle){
-        u8g2.clearBuffer();
-        u8g2.drawBox(0,0, 128, 9);
-        u8g2.setDrawColor(0);
-        u8g2.drawStr(40,8, "Select X/Y");
-        u8g2.drawStr(0, 8, String(X).c_str());
-        u8g2.drawStr(110, 8, String(Y).c_str());
-        u8g2.setDrawColor(1);
-        u8g2.drawFrame(37,10,54,54);
-        u8g2.drawStr(map(posX, 0, 1000, 36, 86),map(posY, 0, 1000, 65, 14), "+");
-        u8g2.drawStr(0,26, String(posX).c_str());
-        u8g2.drawStr(0,36, String(posY).c_str());
-        u8g2.drawStr(108,26, String(posZ).c_str());
-        u8g2.drawStr(0,52, "Con");
-        u8g2.drawStr(4,60, "v");
-        u8g2.drawStr(112,52, "Res");
-        u8g2.drawStr(118,60, "v");
-        u8g2.sendBuffer();
-    }else{
-        u8g2.clearBuffer();
-        u8g2.drawBox(0,0, 128, 9);
-        u8g2.setDrawColor(0);
-        u8g2.drawStr(40,8, "Select Z");
-        u8g2.drawStr(0, 8, String(X).c_str());
-        u8g2.drawStr(110, 8, String(Y).c_str());
-        u8g2.setDrawColor(1);
-        u8g2.drawFrame(58,10,12,54);
-        u8g2.drawStr(61,map(posZ, 0, 1000, 65, 14), "+");
-        u8g2.drawStr(0,26, String(posX).c_str());
-        u8g2.drawStr(0,36, String(posY).c_str());
-        u8g2.drawStr(108,26, String(posZ).c_str());
-        u8g2.drawStr(0,52, "Con");
-        u8g2.drawStr(4,60, "v");
-        u8g2.drawStr(112,52, "Res");
-        u8g2.drawStr(118,60, "v");
-        u8g2.sendBuffer();
-    }
-
-    //Joystick Button
+    //Joystick Button Eingabe
     if(digitalRead(13) == 0){
         if(joystickButtonLast == 0){
-            toggle = !toggle;
+            eingabe = 4;
         }
         joystickButtonLast = 1;
-    }else{
+    }else if(joystickButtonLast == 1){
         joystickButtonLast = 0;
     }
 
-    //ConfirmButton
+    //ConfirmButton Eingabe
     if(digitalRead(0) == 0){
-        if(confirmButtonLast == 0){
-            String msg = (String(posX) + "," + String(posY) + "," + String(posZ));
-            webSocket.sendTXT(msg);
-        }
+        eingabe = 1;
         confirmButtonLast = 1;
-    }else{
+    }else if(confirmButtonLast == 1){
+        eingabe = 2;
         confirmButtonLast = 0;
     }
 
-    //ResetButton
+    //ResetButton Eingabe
     if(digitalRead(2) == 0){
-        if(resetButtonLast == 0){
-            posX = 500;
-            posY = 500;
-            posZ = 500;
-        }
+        eingabe = 5;
         resetButtonLast = 1;
-    }else{
+    }else if(resetButtonLast == 1){
+        eingabe = 6;
         resetButtonLast = 0;
     }
 
-    //Websocket Connection
+    //Joystick Eingabe
+    X = sanitizeInput(changeJoyX(), X);
+    Y = sanitizeInput(changeJoyY(), Y);
+    if((X < 19 || X > 22) || (Y < 19 || Y > 22)){
+        eingabe = 3;
+    }
+
+
+    //Ausgabe ausführen
+    ausgabe = g(zustand, eingabe);
+    switch (ausgabe) {
+        case 1:
+
+            //Werte werden über den WebSocket versendet
+            msg = (String(posX) + "," + String(posY) + "," + String(posZ));
+            webSocket.sendTXT(msg);
+
+            break;
+        case 2:
+
+            //Cursor in die Mitte setzen
+            posX = 500;
+            posY = 500;
+            posZ = 500;
+            //Wenn der Arm "Connected" empfängt gehen die Schrittmotoren auf ihre anfangs Position zurück
+            webSocket.sendTXT("Connected");
+
+            break;
+        case 3:
+
+            //Auf dem Display wird die X/Y Auswahl dargestellt
+            u8g2.clearBuffer();
+            u8g2.drawBox(0,0, 128, 9);
+            u8g2.setDrawColor(0);
+            u8g2.drawStr(40,8, "Select X/Y");
+            u8g2.drawStr(0, 8, String(X).c_str());
+            u8g2.drawStr(110, 8, String(Y).c_str());
+            u8g2.setDrawColor(1);
+            u8g2.drawFrame(37,10,54,54);
+            u8g2.drawStr(map(posX, 0, 1000, 36, 86),map(posY, 0, 1000, 65, 14), "+");
+            u8g2.drawStr(0,26, String(posX).c_str());
+            u8g2.drawStr(0,36, String(posY).c_str());
+            u8g2.drawStr(108,26, String(posZ).c_str());
+            u8g2.drawStr(0,52, "Con");
+            u8g2.drawStr(4,60, "v");
+            u8g2.drawStr(112,52, "Res");
+            u8g2.drawStr(118,60, "v");
+            u8g2.sendBuffer();
+
+            break;
+        case 4:
+
+            //Cursor X/Y Position wird je nach aktueller Joystick Position verändert
+
+            posX = (posX + (X - 21)) > 1000 ? 1000 : (posX + (X - 21)) < 0 ? 0 : (posX + (X - 21) < posX - 1 ||
+                                                                                  posX + (X - 21) > posX + 1) ? posX +
+                                                                                                                (X - 21)
+                                                                                                              : posX; //Von 0 zu 50 zu -2.5 bis +2.5
+            posY = (posY + (Y - 21)) > 1000 ? 1000 : (posY + (Y - 21)) < 0 ? 0 : (posY + (Y - 21) < posY - 1 ||
+                                                                                  posY + (Y - 21) > posY + 1) ? posY +
+                                                                                                                (Y - 21): posY;
+
+            //Aus Automaten Modell sicht sollte sich das Display hier nicht aktualisieren, tut man dies nicht hat man jedoch nur eine sehr geringe Bildwiederholrate von 1/4 FPS
+            u8g2.clearBuffer();
+            u8g2.drawBox(0,0, 128, 9);
+            u8g2.setDrawColor(0);
+            u8g2.drawStr(40,8, "Select X/Y");
+            u8g2.drawStr(0, 8, String(X).c_str());
+            u8g2.drawStr(110, 8, String(Y).c_str());
+            u8g2.setDrawColor(1);
+            u8g2.drawFrame(37,10,54,54);
+            u8g2.drawStr(map(posX, 0, 1000, 36, 86),map(posY, 0, 1000, 65, 14), "+");
+            u8g2.drawStr(0,26, String(posX).c_str());
+            u8g2.drawStr(0,36, String(posY).c_str());
+            u8g2.drawStr(108,26, String(posZ).c_str());
+            u8g2.drawStr(0,52, "Con");
+            u8g2.drawStr(4,60, "v");
+            u8g2.drawStr(112,52, "Res");
+            u8g2.drawStr(118,60, "v");
+            u8g2.sendBuffer();
+
+            break;
+        case 5:
+
+            //Auf dem Display wird die Z Auswahl angezeigt
+            u8g2.clearBuffer();
+            u8g2.drawBox(0,0, 128, 9);
+            u8g2.setDrawColor(0);
+            u8g2.drawStr(40,8, "Select Z");
+            u8g2.drawStr(0, 8, String(X).c_str());
+            u8g2.drawStr(110, 8, String(Y).c_str());
+            u8g2.setDrawColor(1);
+            u8g2.drawFrame(58,10,12,54);
+            u8g2.drawStr(61,map(posZ, 0, 1000, 65, 14), "+");
+            u8g2.drawStr(0,26, String(posX).c_str());
+            u8g2.drawStr(0,36, String(posY).c_str());
+            u8g2.drawStr(108,26, String(posZ).c_str());
+            u8g2.drawStr(0,52, "Con");
+            u8g2.drawStr(4,60, "v");
+            u8g2.drawStr(112,52, "Res");
+            u8g2.drawStr(118,60, "v");
+            u8g2.sendBuffer();
+
+            break;
+        case 6:
+
+            //Cursor Z Position wird je nach aktueller Joystick Position verändert
+
+            posZ = (posZ + (Y - 21)) > 1000 ? 1000 : (posZ + (Y - 21)) < 0 ? 0 : (posZ + (Y - 21) < posZ - 1 ||
+                                                                                  posZ + (Y - 21) > posZ + 1) ? posZ +
+                                                                                                                (Y - 21): posZ;
+
+
+            //Aus Automaten Modell sicht sollte sich das Display hier nicht aktualisieren, tut man dies nicht hat man jedoch nur eine sehr geringe Bildwiederholrate von 1/4 FPS
+            u8g2.clearBuffer();
+            u8g2.drawBox(0,0, 128, 9);
+            u8g2.setDrawColor(0);
+            u8g2.drawStr(40,8, "Select Z");
+            u8g2.drawStr(0, 8, String(X).c_str());
+            u8g2.drawStr(110, 8, String(Y).c_str());
+            u8g2.setDrawColor(1);
+            u8g2.drawFrame(58,10,12,54);
+            u8g2.drawStr(61,map(posZ, 0, 1000, 65, 14), "+");
+            u8g2.drawStr(0,26, String(posX).c_str());
+            u8g2.drawStr(0,36, String(posY).c_str());
+            u8g2.drawStr(108,26, String(posZ).c_str());
+            u8g2.drawStr(0,52, "Con");
+            u8g2.drawStr(4,60, "v");
+            u8g2.drawStr(112,52, "Res");
+            u8g2.drawStr(118,60, "v");
+            u8g2.sendBuffer();
+
+            break;
+        default:
+            break;
+    }
+
+    zustand = f(zustand,eingabe);
+
+    //Websocket Kommunikation am laufen halten
     webSocket.loop();
 
     delay(10);
 }
 
-void convertJoyToPos(){
-    if(toggle) {
-        posX = (posX + (X - 21)) > 1000 ? 1000 : (posX + (X - 21)) < 0 ? 0 : (posX + (X - 21) < posX - 1 ||
-                                                                              posX + (X - 21) > posX + 1) ? posX +
-                                                                                                            (X - 21)
-                                                                                                          : posX; //Von 0 zu 50 zu -2.5 bis +2.5
-        posY = (posY + (Y - 21)) > 1000 ? 1000 : (posY + (Y - 21)) < 0 ? 0 : (posY + (Y - 21) < posY - 1 ||
-                                                                              posY + (Y - 21) > posY + 1) ? posY +
-                                                                                                            (Y - 21): posY;
-    }else{
-        posZ = (posZ + (Y - 21)) > 1000 ? 1000 : (posZ + (Y - 21)) < 0 ? 0 : (posZ + (Y - 21) < posZ - 1 ||
-                                                                              posZ + (Y - 21) > posZ + 1) ? posZ +
-                                                                                                            (Y - 21): posZ;
+//--Ausgabefunktion--
+int g(int z, int e){
+    switch (z) {
+        case 0:
+            switch (e) {
+                case 1: return 1;
+                case 3: return 4;
+                case 4: return 5;
+                case 5: return 2;
+                default: return 3;
+            }
+            break;
+        case 1:
+            switch (e) {
+                case 1: return 1;
+                case 3: return 6;
+                case 4: return 3;
+                case 5: return 2;
+                default: return 5;
+            }
+            break;
+        case 2:
+            switch (e) {
+                case 2: return 3;
+                default: return -1;
+            }
+            break;
+        case 3:
+            switch (e) {
+                case 6: return 5;
+                default: return -1;
+            }
+            break;
+        case 4:
+            switch (e) {
+                case 6: return 3;
+                default: return -1;
+            }
+            break;
+        case 5:
+            switch (e) {
+                case 2: return 5;
+                default: return -1;
+            }
+            break;
     }
+    return -1;
 }
 
+//--Überführungsfunktion--
+int f(int z, int e){
+    switch (z) {
+        case 0:
+            switch (e) {
+                case 1: return 2;
+                case 3: return 0;
+                case 4: return 1;
+                case 5: return 4;
+                default: return z;
+            }
+            break;
+        case 1:
+            switch (e) {
+                case 1: return 5;
+                case 3: return 1;
+                case 4: return 0;
+                case 5: return 3;
+                default: return z;
+            }
+            break;
+        case 2:
+            switch (e) {
+                case 2: return 0;
+                default: return z;
+            }
+            break;
+        case 3:
+            switch (e) {
+                case 6: return 1;
+                default: return z;
+            }
+            break;
+        case 4:
+            switch (e) {
+                case 6: return 0;
+                default: return z;
+            }
+            break;
+        case 5:
+            switch (e) {
+                case 2: return 1;
+                default: return z;
+            }
+            break;
+    }
+    return z;
+}
 
+//--Joystick zu Geschwindigkeit konvertieren--
+//angepasst nach den Werten die wir bei unserer Messreihe herausgefunden haben
 int sanitizeInput(int sanInt, int oldInt){
     int newInt = map(sanInt, 13, 235, 0, 50);
     if(newInt > 50){
@@ -186,11 +395,12 @@ int sanitizeInput(int sanInt, int oldInt){
         newInt = 0;
     }
 
-    if(newInt < oldInt-1 || newInt > oldInt+1){ oldInt = newInt;}
+    if(newInt < oldInt-2 || newInt > oldInt+2){ oldInt = newInt;}
 
     return oldInt;
 }
 
+//--x-Achse wird angeschaltet an A0--
 int changeJoyX(){
     pinMode(14,INPUT);
     pinMode(12,OUTPUT);
@@ -198,6 +408,7 @@ int changeJoyX(){
     return analogRead(A0);
 }
 
+//--y-Achse wird angeschaltet an A0--
 int changeJoyY(){
     pinMode(12,INPUT);
     pinMode(14,OUTPUT);
